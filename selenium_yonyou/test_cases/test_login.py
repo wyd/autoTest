2d01b3d1eb3047fda15afd1d5fbebd58b877deda
wyd <1406152969@qq.com>
2023-12-10 10:26:56 +0800

    add README

--- selenium_yonyou/test_cases/test_login.py
+++ selenium_yonyou/test_cases/test_login.py
@@ -1,6 +1,6 @@
 import pytest
+import yaml
 from selenium import webdriver
-
 from selenium_yonyou.common.base import Base
 from selenium_yonyou.page.login import Login
 from selenium_yonyou.page.main import Main
@@ -8,23 +8,24 @@ from selenium_yonyou.page.main import Main
 
 class TestLogin:
     def setup(self):
-        self.base = Base()
-        my_browser = 'chrome'
-        self.base.open_browser(my_browser)
         my_url = "https://bip-pre.yonyoucloud.com/"
-        self.base.get_url(my_url)
-        self.login = Login(self.base.driver)
+        browser = 'chrome'
+        self.login = Login()
+        self.login.open_browser(browser)
+        self.login.get_url(my_url)
 
-    @pytest.mark.parametrize("data", [("yycyh95@test1988.com", "DGYLznzz230802!")])
-    def test_login_right(self, data):
+    @pytest.mark.parametrize(["username","password"], yaml.safe_load(open("C:\python_workspace\PycharmProjects\pythonProject\selenium_yonyou\data\login.yaml")))
+    def test_login_right(self, username,password):
         # login = Login(self.base.driver)
-        self.result = self.login.goto_main(*data).login_assertion()
+        self.login.login_common(username,password)
+        self.result = self.login.goto_main().login_assertion()
         assert self.result == True
 
-    @pytest.mark.parametrize("data", [("yycyh96@test1988.com", "DGYLznzz230802!")])
-    def test_login_error(self, data):
-        self.result = self.login.goto_main(*data).login_assertion()
-        assert self.result == False
+    # @pytest.mark.parametrize("data", [("yycyh96@test1988.com", "DGYLznzz230802!")])
+    # def test_login_error(self, data):
+    #     self.login.login_common(*data)
+    #     self.result = self.login.goto_main().login_assertion()
+    #     assert self.result == False
 
     def teardown(self):
-        self.base.close_driver()
+        self.login.close_driver()
